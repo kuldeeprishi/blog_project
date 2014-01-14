9e81ebd2515fa37812be02e5d4054f2a966ec65a
Kuldeep Rishi <kuldeepkrishi@gmail.com>
2014-01-14 10:05:38 +0530

    homepage modified to show news

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,11 @@
 from blog.models import Post, Comment
+from news.models import News
 from django.shortcuts import render_to_response
 
 def homepage(request):
 	posts = Post.published_objects.filter(featured=True)[:4]
 	popular_posts = Post.published_objects.all()[:5]
 	comments = Comment.objects.all()[:5]
-	return render_to_response('homepage/index.html', {'posts': posts, 'popular_posts': popular_posts, 'comments': comments})
+	queryset = News.get_published.all()[:4]
+	return render_to_response('homepage/index.html', {'posts': posts, 'popular_posts': popular_posts, 
+						'comments': comments, 'news_list': queryset})
