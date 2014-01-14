52fb638f9f8355e7660eef51e0ea11eb2f7fe2d6
Kuldeep Rishi <kuldeepkrishi@gmail.com>
2014-01-14 09:28:58 +0530

    saved previous changes

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,8 @@
-from blog.models import Post
+from blog.models import Post, Comment
 from django.shortcuts import render_to_response
 
 def homepage(request):
-	posts = Post.published_objects.all()
-	return render_to_response('homepage/index.html', {'posts': posts})
+	posts = Post.published_objects.filter(featured=True)[:4]
+	popular_posts = Post.published_objects.all()[:5]
+	comments = Comment.objects.all()[:5]
+	return render_to_response('homepage/index.html', {'posts': posts, 'popular_posts': popular_posts, 'comments': comments})
