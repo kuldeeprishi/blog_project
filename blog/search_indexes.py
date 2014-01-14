a2a8761beee6135bdf2db3682d9c60a9bc7cd3a9
Irshad <irshad@irshad-moocs.(none)>
2014-01-14 18:05:22 +0530

    search add

---
+++ blog/search_indexes.py
@@ -0,0 +1,14 @@
+import datetime
+from haystack import indexes
+from blog.models import Post
+
+class BlogIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    pub_date = indexes.DateTimeField(model_attr='pub_date')
+
+    def get_model(self):
+        return Post
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
