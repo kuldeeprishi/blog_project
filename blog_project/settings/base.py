871dc5235116ce37f72c8815fe4006503f376a99
Irshad <irshad@irshad-moocs.(none)>
2014-01-16 11:20:56 +0530

    ajax coreected

--- blog_project/settings/base.py
+++ blog_project/settings/base.py
@@ -28,7 +28,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 
-
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
