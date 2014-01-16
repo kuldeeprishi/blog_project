883e996e89bbb2b7d414c2c6a90d36d3997de4b3
Kuldeep Rishi <kuldeepkrishi@gmail.com>
2014-01-16 10:33:58 +0530

    newsletter done

--- blog/views.py
+++ blog/views.py
@@ -25,11 +25,8 @@ def post_archive_index(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
 
-    popular_posts = Post.published_objects.all()[:5]
-    recent_comments = Comment.objects.all()[:4]
     ci = RequestContext(request)
-    return render_to_response('blog/display_post_list.html', {'posts': posts, 'popular_posts': popular_posts, 
-                        'recent_comments': recent_comments,}, ci)
+    return render_to_response('blog/display_post_list.html', {'posts': posts}, ci)
 
 
 class CommentForm(forms.Form):
@@ -42,10 +39,7 @@ def detail_view(request ,year, month, day, slug):
     ci = RequestContext(request)
     form = CommentForm()
     post = get_object_or_404(Post.published_objects, slug=slug)
-    popular_posts = Post.published_objects.all()[:5]
-    recent_comments = Comment.objects.all()[:4]
-    return render_to_response(template_name ,{'form':form, 'post':post, 'popular_posts': popular_posts, 
-                        'recent_comments': recent_comments,} , ci )
+    return render_to_response(template_name ,{'form':form, 'post':post} , ci )
 
 
 @login_required
