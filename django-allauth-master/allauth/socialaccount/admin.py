ddbba67dabf787e153a5a8fb7ddf1133b953444d
Kuldeep Rishi <kuldeepkrishi@gmail.com>
2014-02-01 09:36:27 +0530

    finalized for uploading

---
+++ django-allauth-master/allauth/socialaccount/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+
+from .models import SocialApp, SocialAccount, SocialToken
+
+from ..account import app_settings
+from ..utils import get_user_model
+
+User = get_user_model()
+
+
+class SocialAppAdmin(admin.ModelAdmin):
+    list_display = ('name', 'provider',)
+    filter_horizontal = ('sites',)
+
+
+class SocialAccountAdmin(admin.ModelAdmin):
+    search_fields = ['user__emailaddress__email'] + \
+        list(map(lambda a: 'user__' + a,
+             filter(lambda a: a and hasattr(User(), a),
+                   [app_settings.USER_MODEL_USERNAME_FIELD,
+                    'first_name',
+                    'last_name'])))
+    raw_id_fields = ('user',)
+    list_display = ('user', 'uid', 'provider')
+    list_filter = ('provider',)
+
+
+class SocialTokenAdmin(admin.ModelAdmin):
+    raw_id_fields = ('app', 'account',)
+    list_display = ('app', 'account', 'truncated_token', 'expires_at')
+    list_filter = ('app', 'app__provider', 'expires_at')
+
+    def truncated_token(self, token):
+        max_chars = 40
+        ret = token.token
+        if len(ret) > max_chars:
+            ret = ret[0:max_chars] + '...(truncated)'
+        return ret
+    truncated_token.short_description = 'Token'
+
+admin.site.register(SocialApp, SocialAppAdmin)
+admin.site.register(SocialToken, SocialTokenAdmin)
+admin.site.register(SocialAccount, SocialAccountAdmin)
